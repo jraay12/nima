8c2149c3f5e35a2f8515fad2bba698cd4d008089
John Ray <johnraycaete@gmail.com>
2026-06-04 21:39:10 +0800

    fix

--- src/component/MemberList.tsx
+++ src/component/MemberList.tsx
@@ -665,7 +665,7 @@ export function MemberList({
         </div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-          {filtered.map((member) => (
+          {filtered?.map((member) => (
             <PublicMemberCard
               key={member.id}
               member={member}
